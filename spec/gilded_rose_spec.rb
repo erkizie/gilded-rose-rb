de1a67fab3ba31340438bd3e87f0f29aa2339027
Erik <erkizie@gmail.com>
2019-07-26 18:48:07 +0300

    add tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,6 +1,7 @@
 require 'gilded_rose'
 
 describe GildedRose do
+
   describe '#update_quality' do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
@@ -8,4 +9,15 @@ describe GildedRose do
       expect(items[0].name).to eq 'foo'
     end
   end
+
+  it "decreases the quality value by 1" do
+    items = [Item.new("foo", 5, 10)]
+    expect {GildedRose.new(items).update_quality}.to change {items[0].quality}.by(-1)
+  end
+
+  it "decreases the quality by 2 after the sell_in has reached 0" do
+    items = [Item.new("foo", 0, 10)]
+    expect {GildedRose.new(items).update_quality}.to change {items[0].quality}.by(-2)
+  end
+  
 end
