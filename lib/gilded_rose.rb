5e1e304b8bf2e95db6897ae2dde6e37fad1650c4
Erik <erkizie@gmail.com>
2019-07-26 20:03:49 +0300

    Main file refactoring to separate classes with understandable code

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,53 +1,89 @@
 class GildedRose
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      case
+      when item.name == "normal"
+        items = Normal.new(@items)
+        items.update_quality
+      when item.name == "Aged Brie"
+        items = Brie.new(@items)
+        items.update_quality
+      when item.name == "Sulfuras, Hand of Ragnaros"
+        items = Sulfuras.new(@items)
+        items.update_quality
+      when item.name == "Backstage passes to a TAFKAL80ETC concert"
+        items = Backstage.new(@items)
+        items.update_quality
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+    end
+  end
+end
+
+class Normal
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality()
+    @items.each do |item|
+      item.sell_in -= 1
+      return if item.quality == 0
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in <= 0
+    end
+  end
+end
+
+class Brie
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality
+    @items.each do |item|
+      item.sell_in -= 1
+      return if item.quality >= 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in <= 0
+    end
+  end
+end
+
+class Sulfuras
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality
+  end
+end
+
+class Backstage
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality
+    @items.each do |item|
+      item.sell_in -= 1
+      return if item.quality >= 50
+      return item.quality = 0 if item.sell_in < 0
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 10
+      item.quality += 1 if item.sell_in < 5
     end
   end
 end
